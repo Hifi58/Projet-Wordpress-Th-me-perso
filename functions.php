603c971c9edb27447072be03823bb92479e22125
Hifi58 <gillet.tony@gmail.com>
2021-07-13 11:30:56 +0200

    first commit

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+
+//Ceci ajoute la prise en charge des images mises en avant
+add_theme_support( 'post-thumbnails' );
+
+//Ceci ajout automatiquement le titre du site dans l'en-tête
+
+add_theme_support( 'title-tag' );
